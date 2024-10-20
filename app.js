8e2f4bf4efb1d9448cebecb02f61bb0e3e7c8b46
Himanshi0827 <157987622+Himanshi0827@users.noreply.github.com>
2024-10-20 21:02:30 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ dotenv.config()
 import express from "express";
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
-app.use(cors());
-const app = express()
 
+const app = express()
+app.use(cors());
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
